9418f73a1c1cc8f86eac7b444ad6e92ff676ecc5
Senawguevara <wguevara90@misena.edu.co>
2022-08-11 22:22:59 -0500

    Verificacion Maximo en primiivos

--- Variables/src/Variables/variables.java
+++ Variables/src/Variables/variables.java
@@ -1,4 +1,21 @@
 package Variables;
 
 public class variables {
+    public static void main(String[] args) {
+        String saludar = "Hola Mundo";
+        String saluda = " de nuevo mundo";
+        //atributos
+        System.out.println(saludar);
+        System.out.println("saludar = " + saludar.toUpperCase());
+        System.out.println("saludar.toLowerCase() = " + saludar.toLowerCase());
+        System.out.println("saludar.repeat(3) = " + saludar.repeat(3));
+        System.out.println("saludar.replace(saludar,\"hola\") = " + saludar.replace(saludar,"hola"));
+        System.out.println("saludar.indexOf(0) = " + saludar.indexOf(1));
+        System.out.println("saluda = " + saluda.concat(saluda + saludar));
+        System.out.println("saluda = " + saluda.indent(0));
+
+
+
+    }
+
 }
---
+++ entrada/src/Primitivo.java
@@ -0,0 +1,28 @@
+public class Primitivo {
+    public static void main(String[] args) {
+        byte numeroByte =127 ;
+        System.out.println("numeroByte = " + numeroByte);
+        System.out.println("tipo byte corresponde el Byte a :" + Byte.BYTES);
+        System.out.println("tipo byte coresponde en bites a :" + Byte.SIZE);
+        System.out.println("Valor maximo de un byte es: " + Byte.MAX_VALUE);
+        System.out.println("Valor maximo de un byte es: " + Byte.MIN_VALUE);
+
+        short numeroShort = 32767;
+        System.out.println("numeroShort = " + numeroShort);
+        System.out.println("tipo short corresponde el Byte a :" + Short.BYTES);
+        System.out.println("tipo short coresponde en bites a :" + Short.SIZE);
+        System.out.println("Valor maximo de un Short es: " + Short.MAX_VALUE);
+        System.out.println("Valor maximo de un Short es: " + Short.MIN_VALUE);
+
+        int numeroInt = 2147483647;
+        System.out.println("numeroInt = " + numeroInt);
+        System.out.println("tipo int corresponde el Byte a :" + Integer.BYTES);
+        System.out.println("tipo int coresponde en bites a :" + Integer.SIZE);
+        System.out.println("Valor maximo de un Integer es: " + Integer.MAX_VALUE);
+        System.out.println("Valor maximo de un Integer es: " + Integer.MIN_VALUE);
+
+
+
+
+    }
+}
